9ba9ab1cbb9ab1e98ba5e3dfdc96d8085e47bc74
benkajaja <ny343478@gmail.com>
2020-09-12 23:25:02 +0800

    Add required packages

--- lazy.sh
+++ lazy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt install python3-venv
+sudo apt install -y python3-dev python3-venv
 python3 -m venv env
 ./env/bin/pip3 install -r requirements.txt
 echo "[target]\nserver = " > config.ini
